9af2b03f44d21b0aabf3aba3f3024518809dc19d
onurcanari <arionurcan@gmail.com>
2019-03-16 15:48:21 +0300

    iyileştirmeler

--- StudentManagementSystem/VeritabaniYonetimi.cs
+++ StudentManagementSystem/VeritabaniYonetimi.cs
@@ -9,13 +9,16 @@ public class VeritabaniYonetimi
     SQLiteDataAdapter dat;
     SQLiteConnection con;
 
+    //Dosyamızın varlığından emin olur. Ardından bağlantıyı kurar.
     public VeritabaniYonetimi()
     {
-        // Veritabani dosyamız yoksa oluşturuyor. Ardından içinde tablo oluşturuyor.
+       
         if (!File.Exists("veritabani.sqlite"))
             VeritabaniOlustur();
         con = new SQLiteConnection("Data Source=veritabani.sqlite;Version=3;");
     }
+
+    // Veritabani dosyamız yoksa oluşturuyor. Ardından içinde tablo oluşturuyor.
     public void VeritabaniOlustur()
     {
         SQLiteConnection.CreateFile("veritabani.sqlite");
@@ -32,7 +35,8 @@ public class VeritabaniYonetimi
         con.Close();
     }
 
-    public void YeniKayitEkle(Ogrenci ogrenci)
+    //Veritabanına yeni kayıt ekler.
+    public bool YeniKayitEkle(Ogrenci ogrenci)
     {
         con.Open();
         string sqlSelect = "SELECT TCKimlikNo,Ad,Soyad,OgrenciNo FROM Ogrenciler WHERE TCKimlikNo=@TCKimlikNo OR OgrenciNo=@OgrenciNo";
@@ -44,7 +48,8 @@ public class VeritabaniYonetimi
         // Girilen tckimlikno ile öğrenci no yu kontrol et eğer bir kayda ulaşırsan ekleme
         if (studentExist)
         {
-
+            con.Close();
+            return false;
         }else 
         {
             using (SQLiteTransaction liteTransaction = con.BeginTransaction())
@@ -63,25 +68,29 @@ public class VeritabaniYonetimi
             }
         }
         con.Close();
+        return true;
     }
+
+    // Veritabanından kayıtları çekip bunları  DataTable aracılığıyla döndürür.
     public DataTable KayitlariListele()
     {
         DataSet dataSet = new DataSet();
         string sql = "SELECT * FROM Ogrenciler;";
         con.Open();
-        dat = new SQLiteDataAdapter(sql, con);
+        dat = new SQLiteDataAdapter(sql,con);
         dat.Fill(dataSet, "Ogrenciler");
         con.Close();
         return dataSet.Tables["Ogrenciler"];
     }
 
+    // Veritabanındaki kayıtları istenen özellikler için tarar. Bulduğu kayıtları DataTable aracılığıyla döndürür.
     public DataTable KayitAra(string neyeGore, string ara)
     {
         DataSet dataSet = new DataSet();
         con.Open();
         using(SQLiteCommand com = new SQLiteCommand(con))
         {
-            com.CommandText = string.Format(com.CommandText = "SELECT TCKimlikNo,Ad,Soyad,OgrenciNo FROM Ogrenciler WHERE {0} ='{1}'", neyeGore, ara);
+            com.CommandText = string.Format(com.CommandText = "SELECT TCKimlikNo,Ad,Soyad,OgrenciNo FROM Ogrenciler WHERE lower({0}) ='{1}'", neyeGore, ara);
             //com.CommandText = "SELECT TCKimlikNo,Ad,Soyad,OgrenciNo FROM Ogrenciler WHERE Ad ='@Ara'";
             //com.Parameters.AddWithValue("@NeyeGore", neyeGore);
             //com.Parameters.AddWithValue("@Ara", ara);
